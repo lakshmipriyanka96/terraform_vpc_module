9ee05b807644b269dcb6aed9099e85db5fe97673
llakshmipriyanka96 <llakshmipriyanka1996@gmail.com>
2026-02-13 13:06:39 +0530

    vpc_module

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
-# data "aws_availability_zones" "available" {
-#   state = "available"
-# }
+data "aws_availability_zones" "available" {
+  state = "available"
+}
 
 #  data "aws_vpc" "default" {
 #      default = true
